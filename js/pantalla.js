e390350ccc58bdb8c27b2dea0dbe1f46cd78f0e1
SebastianLagorio <117458445+SebastianLagorio@users.noreply.github.com>
2022-11-22 19:59:30 -0300

    j

--- js/pantalla.js
+++ js/pantalla.js
@@ -64,61 +64,28 @@
 						posy=900
 					}
 				break;
+           }
 
-				case "A":
-					posx=posx-100
-					if(posx<0){
-						posx=0
-					}
-					break;
 			
-				case "D":
-					posx+=100
-					if(posx>900){
-						posx=900
-					}
-				break;
-
-				case "W":
-					posy-=100
-					if(posy<0){
-						posy=0
-					}
-				break;
-
-				case "S":
-					posy+=100
-					if(posy>900){
-						posy=900
-					}
-				break;
-
-				case "F5":
-				break;
-
-				default:
-					alert("Esto no va")
-				break;
-			}
-
-			if (posx == xfan && posy == yfan){
-				console.log("muerte")
-				document.querySelector("#pacman").setAttribute("display", "none");
-				document.querySelector("#muerte").setAttribute("display", "block");	
-			}
-			if (pilx.indexOf(posx + "px")!=(-1) && pily.indexOf(posy + "px")!=(-1)){
-				console.log(posx);
-				console.log(posy);
-				if (pilx.indexOf(posx + "px") == pily.indexOf(posy + "px")){
-					console.log(pilx);
-					console.log(pily);
-					var guarda = pilx.indexOf(posx + "px");
-					pilx.splice(guarda, 1);
-					pily.splice(guarda, 1);
-					puntaje++;
-					document.getElementById("puntaje").innerHTML = puntaje;
-				}
-			}
-
-			pacman.setAttribute("style","top:"+posy+"px; left:"+posx+"px");
+		   pacman.setAttribute("style","top:"+posy+"px; left:"+posx+"px");
+		   
+		   if (posx == xfan && posy == yfan){
+			   console.log("muerte")
+			   document.querySelector("#pacman").style.display='none';
+			   document.querySelector("#muerte").style.display='block';
+		   }
+		   if (pilx.indexOf(posx + "px")!=(-1) && pily.indexOf(posy + "px")!=(-1)){
+			   console.log(posx);
+			   console.log(posy);
+			   console.log("pildora");
+			   if (pilx.indexOf(posx + "px") == pily.indexOf(posy + "px")){
+				   console.log(pilx);
+				   console.log(pily);
+				   var guarda = pilx.indexOf(posx + "px");
+				   pilx.splice(guarda, 1);
+				   pily.splice(guarda, 1);
+				   puntaje++;
+				   document.getElementById("puntaje").innerHTML = puntaje;
+			   }
+		   }
 		}
